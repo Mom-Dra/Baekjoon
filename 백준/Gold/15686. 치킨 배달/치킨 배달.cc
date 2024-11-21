e8b18170f20d283fe65139cd8169a7ece012a2db
Mom-Dra <138591766+Mom-Dra@users.noreply.github.com>
2024-11-21 09:29:44 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,81 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <limits>
 
-int GetChikenDistance(const std::vector<std::vector<int>>& map, int r, int c)
+int GetChikenDistance(const std::vector<std::pair<int, int>>& chikens, const std::pair<int, int>& home)
 {
-	int min{ std::numeric_limits<int>::max() };
-
-	for (int i{ 0 }; i < map.size(); ++i)
-	{
-		for (int j{ 0 }; j < map[0].size(); ++j)
-		{
-			if (map[i][j] == 2)
-			{
-				int distance{ std::abs(r - i) + std::abs(c - j) };
-				min = std::min(min, distance);
-			}
-		}
-	}
+    int min{std::numeric_limits<int>::max()};
+    
+    for(const auto& chiken : chikens)
+    {
+        int dis{ std::abs(chiken.first - home.first) + std::abs(chiken.second - home.second) };
+        min = std::min(min, dis);
+    }
 
 	return min;
 }
 
-int GetCityChikenDistance(const std::vector<std::vector<int>>& map)
+int GetCityChikenDistance(const std::vector<std::pair<int, int>>& chikens, const std::vector<std::pair<int, int>>& homes)
 {
-	int distance{ 0 };
-
-	for (int i{ 0 }; i < map.size(); ++i)
-	{
-		for (int j{ 0 }; j < map[0].size(); ++j)
-		{
-			if (map[i][j] == 1)
-			{
-				distance += GetChikenDistance(map, i, j);
-			}
-		}
-	}
-
+    int distance{0};
+    
+    for(const auto& home : homes)
+    {
+        distance += GetChikenDistance(chikens, home);
+    }
+    
 	return distance;
 }
 
-int SelectChiken(std::vector<std::vector<int>>& map, int r, int c, int count, int m)
+int BackTrack(const std::vector<std::pair<int, int>>& chikens, const std::vector<std::pair<int, int>>& homes, std::vector<std::pair<int, int>>& included, int curr, int m)
 {
-	// m개만 남긴다!
+    if(included.size() == m)
+    {
+        return GetCityChikenDistance(included, homes);
+    }
+    
+    int min{std::numeric_limits<int>::max()};
+    
+    for(int i{curr}; i < chikens.size(); ++i)
+    {
+        included.emplace_back(chikens[i].first, chikens[i].second);
+        min = std::min(min, BackTrack(chikens, homes, included, i + 1, m));
+        included.pop_back();
+    }
+    
+    return min;
+}
 
-	if (count == m)
-	{
-		return GetCityChikenDistance(map);
-	}
-
-	int min{ std::numeric_limits<int>::max() };
-
-	for (int i{ r }; i < map.size(); ++i)
-	{
-		for (int j{ i == r ? c : 0 }; j < map[0].size(); ++j)
-		{
-			if (map[i][j] == 2)
-			{
-				map[i][j] = 0;
-
-				int ni{ i };
-				int nj{ j + 1 };
-				
-				if (nj == map[0].size())
-				{
-					nj = 0;
-					++ni;
-				}
-
-				min = std::min(min, SelectChiken(map, ni, nj, count + 1, m));
-
-				map[i][j] = 2;
-			}
-		}
-	}
-
-	return min;
+int Solve(const std::vector<std::pair<int, int>>& chikens, const std::vector<std::pair<int, int>>& homes, int m)
+{
+    std::vector<std::pair<int, int>> included;
+    
+    return BackTrack(chikens, homes, included, 0, m);
 }
 
 int main()
@@ -88,20 +65,33 @@ int main()
 	std::cin >> n >> m;
 
 	std::vector<std::vector<int>> arr(n, std::vector<int>(n, 0));
-
-	int chikenCount{ 0 };
-
-	for (auto& vec : arr)
+	
+	std::vector<std::pair<int, int>> chikens;
+	std::vector<std::pair<int, int>> homes;
+	
+	for(int i{0}; i < n; ++i)
 	{
-		for (auto& num : vec)
-		{
-			std::cin >> num;
-
-			if (num == 2) ++chikenCount;
-		}
+	    for(int j{0}; j < n; ++j)
+	    {
+	        int num;
+	        std::cin >> num;
+	        
+	        arr[i][j] = num;
+	        
+	        switch(num)
+	        {
+            case 1:
+	            homes.emplace_back(i, j);
+	            break;
+	       
+	        case 2:
+	            chikens.emplace_back(i, j);
+	            break;
+	        }
+	    }
 	}
-
-	std::cout << SelectChiken(arr, 0, 0, 0, chikenCount - m);
-
+	
+	std::cout << Solve(chikens, homes, m);
+	
 	return 0;
 }

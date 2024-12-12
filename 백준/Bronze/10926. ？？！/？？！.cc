9f36e91b8b509e20407867c1afd2ce8c2dbcf832
Mom-Dra <138591766+Mom-Dra@users.noreply.github.com>
2024-12-12 09:51:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+int main()
+{
+	std::string s;
+	std::cin >> s;
+
+	std::cout << s << "??!";
+
+	return 0;
+}

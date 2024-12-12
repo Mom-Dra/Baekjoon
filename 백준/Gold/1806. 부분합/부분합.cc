ec5119c33ba068e8713e49fb447fcd6b22b2ac81
Mom-Dra <138591766+Mom-Dra@users.noreply.github.com>
2024-12-12 17:48:53 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <limits>
+
+int Solve(const std::vector<int>& arr, const int N, const int S)
+{
+    int left{0};
+    int right{1};
+
+    bool canSum{false};
+    int min{std::numeric_limits<int>::max()};
+    int sum{arr[0]};
+
+    while(left < right)
+    {
+        if(sum >= S)
+        {
+            canSum = true;
+            
+            min = std::min(min, right - left);
+        }
+
+        if(sum < S)
+        {
+            if(right == N) break;
+            sum += arr[right];
+            ++right;
+        }
+        else
+        {
+            sum -= arr[left];
+            ++left;
+        }
+    }
+            
+    if(!canSum) return 0;
+    return min;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int N, S;
+    std::cin >> N >> S;
+
+    std::vector<int> arr(N);
+    
+    for(auto& num : arr)
+        std::cin >> num;
+    
+    std::cout << Solve(arr, N, S);
+
+    return 0;
+}

93434f31e27cb26ea15f32c8a3636e9959f29515
Mom-Dra <138591766+Mom-Dra@users.noreply.github.com>
2024-12-12 10:26:00 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int byte;
+	std::cin >> byte;
+
+	for (int i{ 0 }; i < byte / 4; ++i)
+	{
+		std::cout << "long ";
+	}
+
+	std::cout << "int";
+
+	return 0;
+}

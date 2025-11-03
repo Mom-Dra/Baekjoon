defb7ac3276aac3f6b93322da906036271f01500
Mom-Dra <138591766+Mom-Dra@users.noreply.github.com>
2025-11-03 17:37:11 +0900

    [Gold III] Title: 드래곤 커브, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -2,71 +2,67 @@
 #include <vector>
 #include <array>
 
+void drawDragonCurve(int x, int y, int d, int g, std::array<std::array<bool, 101>, 101>& map)
+{
+	static std::array<int, 4> dx{ 1, 0, -1, 0 };
+	static std::array<int, 4> dy{ 0, -1, 0, 1 };
+
+	std::vector<int> dirs;
+	dirs.push_back(d);
+
+	for (int gen{ 0 }; gen < g; ++gen)
+	{
+		int currSize = dirs.size();
+
+		for (int i{ currSize - 1 }; i >= 0; --i)
+		{
+			dirs.push_back((dirs[i] + 1) % 4);
+		}
+	}
+
+	map[y][x] = true;
+
+	for (int dir : dirs)
+	{
+		x += dx[dir];
+		y += dy[dir];
+
+		if (x >= 0 && x <= 100 && y >= 0 && y <= 100)
+			map[y][x] = true;
+	}
+}
+
 int main()
 {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	std::array<std::array<bool, 101>, 101> map{};
+
 	int N;
 	std::cin >> N;
-
-	std::array<std::array<bool, 101>, 101> visited{};
-	std::array<std::pair<int, int>, 4> dn{ {{ 1, 0 }, {0, -1}, {-1, 0}, {0, 1}} };
 
 	for (int i{ 0 }; i < N; ++i)
 	{
 		int x, y, d, g;
 		std::cin >> x >> y >> d >> g;
 
-		std::vector<std::pair<int, int>> vertices;
-		vertices.emplace_back(x, y);
-		visited[x][y] = true;
-
-		std::pair<int, int> referencePoint{ x, y };
-
-		referencePoint.first = x + dn[d].first;
-		referencePoint.second = y + dn[d].second;
-
-		vertices.emplace_back(referencePoint);
-		visited[referencePoint.first][referencePoint.second] = true;
-
-		for (int j{ 0 }; j < g; ++j)
-		{
-			std::vector<std::pair<int, int>> tmpVertices{ vertices };
-
-			for (auto& [x, y] : tmpVertices)
-			{
-				x -= referencePoint.first;
-				y -= referencePoint.second;
-
-				int rotatedX = -y;
-				int rotatedY = x;
-
-				x = rotatedX + referencePoint.first;
-				y = rotatedY + referencePoint.second;
-			}
-
-			for (size_t i{ 0 }, j{ tmpVertices.size() - 1 }; i < tmpVertices.size(); ++i, --j)
-			{
-				const auto& [x, y] {tmpVertices[j]};
-
-				visited[x][y] = true;
-				vertices.emplace_back(x, y);
-			}
-
-			referencePoint = tmpVertices[0];
-		}
+		drawDragonCurve(x, y, d, g, map);
 	}
 
 	int count{ 0 };
 
-	for (int i{ 0 }; i < 100; ++i)
+	for (int y{ 0 }; y < 100; ++y)
 	{
-		for (int j{ 0 }; j < 100; ++j)
+		for (int x{ 0 }; x < 100; ++x)
 		{
-			if (visited[i][j] && visited[i + 1][j] && visited[i][j + 1] && visited[i + 1][j + 1])
+			if (map[y][x] && map[y + 1][x] && map[y][x + 1] && map[y + 1][x + 1])
 				++count;
 		}
 	}
 
-	std::cout << count;
+	std::cout << count << '\n';
 
 	return 0;
 }
